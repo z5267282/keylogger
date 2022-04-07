39f2645d73ccaf081ffd69e8b5a790cc7d9ffc18
Sunny Chen <z5267282@student.unsw.edu.au>
2022-04-08 00:42:20 +1000

    Adding .gitignore in testing

--- keyboard-module.py
+++ keyboard-module.py
@@ -5,6 +5,8 @@ import keyboard
 """
 
 # Record events until 'esc' is pressed.
-recorded = keyboard.record(until='esc')
-# Then replay back at three times the speed.
-keyboard.play(recorded, speed_factor=3)
+# recorded = keyboard.record(until='esc')
+# # Then replay back at three times the speed.
+# keyboard.play(recorded, speed_factor=3)
+
+print('hello')
