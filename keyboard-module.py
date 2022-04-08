6a4da8b060db3e9e7e6b723fdbc1d324d11e7933
Sunny Chen <z5267282@student.unsw.edu.au>
2022-04-08 22:36:09 +1000

    Trying out pynput module

--- keyboard-module.py
+++ keyboard-module.py
@@ -4,7 +4,22 @@ import keyboard
     Playing around with: https://pypi.org/project/keyboard/
 """
 
-# Record events until 'esc' is pressed.
-recorded = keyboard.record(until='esc')
-# Then replay back at three times the speed.
-keyboard.play(recorded, speed_factor=3)
+def record_test():
+    # Record events until 'esc' is pressed.
+    recorded = keyboard.record(until='esc')
+
+    keyboard.play(recorded)
+
+def write_test():
+    keyboard.write('Fishing in the river!\n', delay=0.1)
+
+def strings_test():
+    recorded = keyboard.record(until='esc')
+
+    print(list(keyboard.get_typed_strings(recorded)))
+
+
+if __name__ == '__main__':
+    record_test()
+    # write_test()
+    # strings_test()
