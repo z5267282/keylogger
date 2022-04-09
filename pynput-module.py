3d5ab4fc126011ebb4be272e8d9050a4fbd4c699
Sunny Chen <z5267282@student.unsw.edu.au>
2022-04-09 17:05:42 +1000

    pynput module initial testing

---
+++ pynput-module.py
@@ -0,0 +1,26 @@
+from pynput import keyboard
+
+def on_press(key):
+    try:
+        print(key.char, end='')
+    except AttributeError:
+        print(key, end='')
+
+def on_release(key):
+    if key == keyboard.Key.esc:
+        # In the callback this is how we stop the listener
+        return False
+
+def with_statement():
+    with keyboard.Listener(
+            on_press=on_press,
+            on_release=on_release) as listener:
+        listener.join()
+
+def no_with():
+    # the non-blocking approach: we want to track other inputs later
+    listener = keyboard.Listener(
+        on_press=on_press,
+        on_release=on_release
+    )
+    listener.start()
