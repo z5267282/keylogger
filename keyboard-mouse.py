9c6f6b4eb7cb42b6847384a447720d11c07ae910
Sunny Chen <z5267282@student.unsw.edu.au>
2022-04-12 14:38:52 +1000

    Pressing keys not responsive

--- keyboard-mouse.py
+++ keyboard-mouse.py
@@ -8,23 +8,21 @@ def on_press(key):
     try:
         print(key.char, end='')
     except AttributeError:
-        print(key, end='')
-
-def on_release(key):
-    # Just escape for now
-    if key == keyboard.Key.esc:
-        done = True
-        return False
+        if key == keyboard.Key.esc:
+            global done
+            done = True
+            return False
 
 def on_click(x, y, button, pressed):
     print(f'({x},{y}), {button}, {pressed}')
 
-k = keyboard.Listener(on_press=on_press, on_release=on_release)
+k = keyboard.Listener(on_press=on_press)
 k.start()
-k.join()
 
 m = mouse.Listener(on_click=on_click)
 m.start()
-m.join()
-if done:
-    m.stop()
+
+while not done:
+    pass
+
+m.stop()
