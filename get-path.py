44eb4a2ac37cc3580130023de51c912a262115ef
Sunny Chen <z5267282@student.unsw.edu.au>
2022-04-16 15:45:17 +1000

    Testing that absolute path finding works with the os module

---
+++ get-path.py
@@ -0,0 +1,6 @@
+import os
+import sys
+
+# following steps from: https://stackoverflow.com/questions/595305/how-do-i-get-the-path-of-the-python-script-i-am-running-in
+
+print(os.path.abspath(sys.argv[0]))
