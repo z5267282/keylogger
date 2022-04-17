483516e5271e1b35f18dbb011b2a59dd36eba620
Sunny Chen <z5267282@student.unsw.edu.au>
2022-04-17 14:23:57 +1000

    Testing email checking algorithm

---
+++ email-check.py
@@ -0,0 +1,8 @@
+import re
+
+
+def is_email(email):
+    regex = re.compile(r"([-!#-'*+/-9=?A-Z^-~]+(\.[-!#-'*+/-9=?A-Z^-~]+)*|\"([]!#-[^-~ \t]|(\\[\t -~]))+\")@([-!#-'*+/-9=?A-Z^-~]+(\.[-!#-'*+/-9=?A-Z^-~]+)*|\[[\t -Z^-~]*])")
+    return re.match(regex, email)
+
+print(bool(is_email('test@gmaio.com')))
